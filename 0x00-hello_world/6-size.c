af1f6a891df1c433c9f7be378b4ac2d1217ad128
Denis Seronu <seronodenis@gmail.com>
2022-06-30 06:20:24 -0700

    Starting low level programming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,9 +7,9 @@
 
 int main(void)
 {
-	print("The size of a character: %d byte(s)\n", sizeof(char));
-	print("The size of an integer: %d byte(s)\n", sizeof(int));
-	print("The size of a float: %d byte(s)\n", sizeof(float));
-	print("The size of a double data type : %d byte(s)\n", sizeof(double));
+	printf("The size of a character: %d byte(s)\n", sizeof(char));
+	printf("The size of an integer: %d byte(s)\n", sizeof(int));
+	printf("The size of a float: %d byte(s)\n", sizeof(float));
+	printf("The size of a double data type : %d byte(s)\n", sizeof(double));
 	return (0);
 }
