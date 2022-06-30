bea1389b7fe9241586323b577efc5b1cff6b4c01
Denis Seronu <seronodenis@gmail.com>
2022-06-30 05:23:32 -0700

    Starting low level programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
 
-/* Program to return zero 
- * at the end */
+/*
+ * Program to return zero 
+ * at the end 
+ */
 
-int main(void){
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
