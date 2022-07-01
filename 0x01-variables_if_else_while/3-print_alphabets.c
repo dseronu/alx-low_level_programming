e889a5dac960b3dd65f142954eb551bf36e38900
Denis Seronu <seronodenis@gmail.com>
2022-07-01 09:37:21 -0700

    Variable, if else, while

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Main entry block
+ * Description: print uppercase and lowercae alphabets
+ * Return: Always o when successful
+ */
+int main (void)
+{
+	char c="a";
+	char C="A";
+	
+	while(c <= 'z')
+	{
+		putchar(c);
+		c++
+	}
+	while(C <= 'Z')
+	{
+		putchar(C);
+		C++
+	}
+	putchar('\n');
+	return (0);
+}
