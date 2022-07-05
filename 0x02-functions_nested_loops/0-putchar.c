cd62d282c3aad5a0da4e54d13118184c293ee038
Denis Seronu <seronodenis@gmail.com>
2022-07-05 01:45:42 -0700

    Functions and nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Main entry block
+ * Description: prints _putchar followed by a new line
+ * Return: Alwats 0
+ */
+
+int main(void)
+{
+	char c[10] = "_putchar";
+	int i;
+
+	for (i=0; i < 8; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Project on C functions and nested loops
