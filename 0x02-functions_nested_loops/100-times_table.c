ff4129b37fa66270ec4a0411af5c7f066b10565b
Denis Seronu <seronodenis@gmail.com>
2022-07-05 05:05:34 -0700

    Functions and nested loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,44 +1,51 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * print_times_table - Prints times table
- * Description: Function that prints the n times table
- * Return: 0
+ *  print_times_table - prints the times table
+ *  @n: integer for which the times table will be printed
+ *  Description: prints the times table
+ *  Return: void
  */
 
 void print_times_table(int n)
 {
-	int digit, mult, result;
-
-	if (n <= 15 && n >= 0)
-	{
-		for (digit = 0; digit <= n; digit++)
-		{
-			_putchar('0');
-
-			for (mult = 1; mult <= n; mult++)
-			{
-				_putchar(',');
-				_putchar(' ');
-				result = digit * mult;
-
-				if (result <= 99)
-					_putchar(' ');
-
-				if (result <= 9)
-					_putchar(' ');
+	int row, column, product;
 	
-				if (result >= 100)
+	if (n >= 0 && n < 15)
+	{
+		for (row = 0; row <= n; row++)
+		{
+			for (column = 0; column <= n; column++)
+			{
+				product = row * column;
+				if (column == 0)
 				{
-					_putchar((result / 100) + '0');
-					_putchar((result / 10) % 10 + '0');
+					_putchar('0');
 				}
-
-				else if (result <= 99 && result >= 10)
+				else if (product < 10)
 				{
-					_putchar((result / 10) + '0');
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(product % 10 + '0');
 				}
-				_putchar((result % 10) + '0');
+				else if (product >= 10 && product < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((product / 10) % 10 + '0');
+					_putchar(product % 10 + '0');
+				}
+				else if (product > 99 && product < 1000)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(product / 100 + '0');
+					_putchar((product / 10) % 10 + '0');
+					_putchar(product % 10 + '0');
+				}
 			}
 			_putchar('\n');
 		}
