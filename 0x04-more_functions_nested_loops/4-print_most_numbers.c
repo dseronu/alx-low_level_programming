9dfaf3beeacd710b122396acf6b7d2265d7bc678
Denis Seronu <seronodenis@gmail.com>
2022-07-07 15:37:00 +0300

    more functions and nested loops

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,11 +11,10 @@ void print_most_numbers(void)
 
 	for (i = 48; i < 58; i++)
 	{
-		if (i == 50 && i == 52)
+		if (i != 50 && i != 52)
 		{
-			continue;
+		 	_putchar(i);
 		}
-		 _putchar(i);
 	}
 	_putchar('\n');
 }
