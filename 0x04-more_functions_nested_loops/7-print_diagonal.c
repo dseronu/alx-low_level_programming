502131817f1e8e4913800d11efd3b7835d3684d9
Denis Seronu <seronodenis@gmail.com>
2022-07-07 16:14:05 +0300

    more functions and nested loops

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,23 +8,22 @@
 
 void print_diagonal(int n)
 {
-	int i = 0;
-	int j = 0;
-
-	while (i < n)
-	{
-		while (j <= i)
-		{
-			if (j != i)
-				_putchar(' ');
-			else
-				_putchar(92);
-			j += 1;
-		}
-		_putchar('\n');
-		i += 1;
-		j = 0;
-	}
+	int i, space;
+	
 	if (n <= 0)
+	{
 		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (space = 0; space < i; space++)
+			{
+				_putchar(' ');
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
 }
