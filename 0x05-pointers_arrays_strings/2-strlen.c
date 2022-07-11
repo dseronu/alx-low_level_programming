bdef0d649cf2edfdd9dfc06bb6d25e5d0089a250
Denis Seronu <seronodenis@gmail.com>
2022-07-11 15:35:12 +0300

    pointers, array and strings project

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strlen - length of a string
+ * _strlen - function that returns the length of a string
  * @s: input char
  * Return: length of a string
  */
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - function that prints a string, followed by a \n
+ * @str: input char
+ * Return: string and new line
+ */
+
+void _puts(char *str)
+{
+	str = "Hello world";
+
+	_puts(str);
+
+	return (0);
+}
