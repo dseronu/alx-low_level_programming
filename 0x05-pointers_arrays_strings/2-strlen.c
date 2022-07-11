149a5691f98cb45bd0413e4da0354b6abb0e9799
Denis Seronu <seronodenis@gmail.com>
2022-07-11 15:11:34 +0300

    pointers, array and strings project

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * int_strlen - function that returns the len of string
+ * @*s: string
+ * return: always 0
+ */
+
+int_strlen(char *s)
+{
+	int len;
+
+	len = strlen(*s[6]="Hello");
+	return (0);
+}
