1a468e86519479552ae88fb9a5f8e940b68b24e9
Denis Seronu <seronodenis@gmail.com>
2022-07-14 21:11:22 +0300

    Pointer, arrays and strings

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,20 +8,41 @@
 
 char *leet(char *s)
 {
-	char alphaArr[] = "a4A4e3E3o0O0t7T7l1L1";
-	int i;
-	int j;
+	int i = 0, j = 0;
+	char rr[2][10];
 
-	for (i = 0; s[i] != '\0'; i++)
+	rr[0][0] = 'a';
+	rr[0][1] = 'e';
+	rr[0][2] = 't';
+	rr[0][3] = 'l';
+	rr[0][4] = 'o';
+	rr[0][5] = 'A';
+	rr[0][6] = 'E';
+	rr[0][7] = 'T';
+	rr[0][8] = 'L';
+	rr[0][9] = 'O';
+	rr[1][0] = '4';
+	rr[1][1] = '3';
+	rr[1][2] = '7';
+	rr[1][3] = '1';
+	rr[1][4] = '0';
+	rr[1][5] = '4';
+	rr[1][6] = '3';
+	rr[1][7] = '7';
+	rr[1][8] = '1';
+	rr[1][9] = '0';
+
+	while (s[i])
 	{
-		for (j = 0; alphaArr[j] != '\0'; j++)
+		j = 0;
+		while (j < 10)
 		{
-			if (s[i] == alphaArr[j])
-			{
-				s[i] = alphaArr[j + 1];
-				break;
-			}
+			if (s[i] == rr[0][j])
+				s[i] = rr[1][j];
+			j++;
 		}
+		i++;
 	}
+
 	return (s);
 }
