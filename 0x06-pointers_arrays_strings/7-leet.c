06e666f5135c554d4e8cd80d2ba234f8f804fdde
Denis Seronu <seronodenis@gmail.com>
2022-07-14 21:14:32 +0300

    Pointer, arrays and strings

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,43 +6,23 @@
  * Return: char 
  */
 
-char *leet(char *s)
+char *leet(char *c)
 {
-	int i = 0, j = 0;
-	char rr[2][10];
+	char *cp = c;
+	char k[] = {'A', 'E', 'O', 'T', 'L'};
+	int v[] = {4, 3, 0, 7, 1};
+	unsigned int i;
 
-	rr[0][0] = 'a';
-	rr[0][1] = 'e';
-	rr[0][2] = 't';
-	rr[0][3] = 'l';
-	rr[0][4] = 'o';
-	rr[0][5] = 'A';
-	rr[0][6] = 'E';
-	rr[0][7] = 'T';
-	rr[0][8] = 'L';
-	rr[0][9] = 'O';
-	rr[1][0] = '4';
-	rr[1][1] = '3';
-	rr[1][2] = '7';
-	rr[1][3] = '1';
-	rr[1][4] = '0';
-	rr[1][5] = '4';
-	rr[1][6] = '3';
-	rr[1][7] = '7';
-	rr[1][8] = '1';
-	rr[1][9] = '0';
-
-	while (s[i])
+	while (*c)
 	{
-		j = 0;
-		while (j < 10)
-		{
-			if (s[i] == rr[0][j])
-				s[i] = rr[1][j];
-			j++;
-		}
-		i++;
+		for (i = 0; i < sizeof(k) / sizeof(char); i++)
+	{
+		if (*c == k[i] || *c == k[i] + 32)
+	{
+		*c = 48 + v[i];
 	}
-
-	return (s);
+	}
+		c++;
+	}
+	return (cp);
 }
