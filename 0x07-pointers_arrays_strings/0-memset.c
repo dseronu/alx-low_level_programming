dcebc90825bf9891aa7442664fb16b89e448ae73
Denis Seronu <seronodenis@gmail.com>
2022-07-18 14:38:56 +0300

    Pointers, array and strings

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,14 +10,12 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-
 	unsigned int i;
 
-	i = 0;
-	while (i < n)
+	for (i = 0; n > 0 ; i++, n--)
 	{
 		s[i] = b;
-		i++;
 	}
-	return (s)
+
+	return (s);
 }
