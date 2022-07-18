c0fa655c1fc0bec64f786f207501c7bc5c78d143
Denis Seronu <seronodenis@gmail.com>
2022-07-18 16:50:18 +0300

    Pointers, array and strings

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string
+ * @c: character
+ * Return: pointer to the first occurrence
+ */
+
+char *_strchr(char *s, char c)
+{
+
+}
