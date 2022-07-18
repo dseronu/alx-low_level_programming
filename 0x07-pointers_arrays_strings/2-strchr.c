cdbc32bd05b2ec19f761ce71425bd62a2d03088c
Denis Seronu <seronodenis@gmail.com>
2022-07-18 17:05:35 +0300

    Pointers, array and strings

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,10 +3,10 @@
 /**
  * _strchr - locates a character in a string
  * @s: string to check
- * @c: character to check 
+ * @c: character to check for
+ *
  * Return: pointer to spot in s with c or null
  */
-
 char *_strchr(char *s, char c)
 {
 	int i;
