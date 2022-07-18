64b2853a7c25a045b4c64c0b7c4a6f0e06758490
Denis Seronu <seronodenis@gmail.com>
2022-07-18 19:06:43 +0300

    Pointers, array and strings

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
 /**
- * set_string - sets the value of a pointer to a char.
- * @s: double pointer to be copied
- * @to: where s gets set to
+ * set_string - sets the value of a pointer to a char
+ * @s: pointer to change
+ * @to: string to change pointer to
+ *
+ * Return: void
  */
 void set_string(char **s, char *to)
 {
-	char str;
-
-	*to = str;
-	**s = *to;
+	*s = to;
 }
