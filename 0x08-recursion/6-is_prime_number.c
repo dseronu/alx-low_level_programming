aec05dde7c5745c788b1128074ee64ba554adc73
Denis Seronu <seronodenis@gmail.com>
2022-07-20 13:12:07 +0300

    Recursion

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * is_prime_number - function to check prime numbers
+ * @i: first integer
+ * @j: second integer
+ * Return: integer
+ */
+
+int check_num(int i, int j)
+{
+	if (j < 2 || j % i == 0)
+	{
+		return (0);
+	}
+	else if ((i > j) / 2)
+	{
+		return (1);
+	}
+	else
+	{
+		return (check(i + 1; j));
+	}
+}
+
+/**
+ * is_prime_number - function to check prime numbers
+ * @n: integer
+ * Return: integer
+ */
+
+int is_prime_number(int n)
+{
+	if (n == 2)
+	{
+		return (1);
+	}	
+	return (check(2, n));
+}
