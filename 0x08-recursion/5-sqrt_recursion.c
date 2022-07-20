84318e46fb404e0fe6aea41f42c51175a044a839
Denis Seronu <seronodenis@gmail.com>
2022-07-20 13:00:31 +0300

    Recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - functions that returns the natural square root
- * @n: integer
- * Return: square root
+ * check - checks square root of a number
+ * @i: first integer
+ * @j: second intger
+ * Return: integer
  */
 
 int check(int i, int j)
@@ -18,6 +19,12 @@ int check(int i, int j)
 	}
 	return (check(i + 1, j));
 }
+
+/**
+ * _sqrt_recursion - functions that returns the natural square root
+ * @n: integer
+ * Return: square root
+ */
 
 int _sqrt_recursion(int n)
 {
