cad4ad7dcd028bf5e263d55631ba71a4baf39125
Denis Seronu <seronodenis@gmail.com>
2022-07-22 15:31:55 +0300

    argc and argv in programming language

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,8 +7,11 @@
  * Return: 0
  */
 
-int main(int argc, char agrv[])
+int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[argc - 1]);
+	if (argc == 1)
+	{
+		printf("%s\n", argv[0]);
+	}
 	return (0);
 }
