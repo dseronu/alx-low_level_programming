8495b3528f795b8e24e494ad53557e090fc8e95b
Denis Seronu <seronodenis@gmail.com>
2022-07-26 21:48:12 +0300

    malloc & free in C programming

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,6 +12,7 @@
 int _strlen(char *s)
 {
 	int size = 0;
+	
 	for (; s[size] != '\0'; size++)
 		;
 	return (size);
@@ -33,9 +34,10 @@ char *str_concat(char *s1, char *s2)
 		s1 = "\0";
 	if (s2 == NULL)
 		s2 = "\0";
-
+	
 	size1 = _strlen(s1);
 	size2 = _strlen(s2);
+	
 	m = malloc((size1 + size2) *sizeof(char) + 1);
 	if (m == 0)
 		return (0);
