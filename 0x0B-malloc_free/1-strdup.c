42ae4afb30683303dcb281453e61c311ef9c3fcb
Denis Seronu <seronodenis@gmail.com>
2022-07-26 21:40:47 +0300

    malloc & free in C programming

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -6,29 +6,29 @@
  * _strdup - Return pointer to a new string that duplicates given string,
  * allocate mem w/ malloc
  * @str: String to duplicate
- *
  * Return: Pointer to new string, NULL if failed to make memory
  */
 char *_strdup(char *str)
 {
-	char *newStr;
-	unsigned int i, len;
+	char *s;
+	int i = 0, j;
 
-	if (str == NULL)
+	if (!str)
 		return (NULL);
-	i = len = 0;
-	while (str[len] != '\0')
-	{
-		len++;
-	}
-	len++;
-	newStr = malloc(len * sizeof(*str));
-	if (newStr == NULL)
-		return (NULL);
-	while (i <= len)
-	{
-		newStr[i] = str[i];
+
+	while (*(str + i))
 		i++;
+	i++;
+	s = malloc(sizeof(char) * i);
+
+	if (s == NULL)
+	{
+		return (NULL);
 	}
-	return (newStr);
-}}
+
+	for (j = 0; j <= i; j++)
+	{
+		s[j] = str[j];
+	}
+	return (s);
+}
