1de3d4f95f1c66779a22186ddcfd8cb022a76613
Denis Seronu <seronodenis@gmail.com>
2022-07-29 00:15:46 +0300

    More malloc, free

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,18 +1,22 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+
 /**
- * malloc_checked - creates an array of integers.
- * @b: input.
- * Return: Output.
+ * malloc_checked - fuction that allocates memory and also checks if sucessful
+ * @b: size of memory to allocate.
+ *
+ * Return: pointer to memory, Else if return exit status 98.
  */
 void *malloc_checked(unsigned int b)
 {
-	void *g = malloc(b);
+	void *ptr;
 
-	if (g == NULL)
+	ptr = malloc(b);
+	if (ptr == NULL)
 	{
+		free(ptr);
 		exit(98);
 	}
-	return (g);
+	return (ptr);
 }
