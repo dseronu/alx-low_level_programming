288bfb221b8db62c88d911c5f45ca6fdad1b1e3d
Denis Seronu <seronodenis@gmail.com>
2022-08-02 12:57:50 -0700

    CPreprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
-/*
- * main - print the name of the file that was compiled
- * Return: 0
+#define FILE_NAME __FILE__
+/**
+ * main - print the name of the file the program is compiled from.
+ *
+ * Return: Always 0.
  */
+
 int main(void)
+
 {
-	printf("%s\n", __FILE__);
+	printf("%s\n", FILE_NAME);
 	return (0);
 
 }
